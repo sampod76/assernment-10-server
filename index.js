bd70733ef948e766f74f5f74209c715991b84c08
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-10-25 10:06:27 +0600

    cors use and listen port

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ const cors =require ('cors');
 
 const app=express();
 
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('this is home page')
+});
+
+
+
+
+
+app.listen(port ,()=>{
+    console.log('The server run 5000 port')
+})
 
